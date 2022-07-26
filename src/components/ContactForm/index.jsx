e2e4108b219d316e7f077f8e29183f2dbe7684e2
Antonio Gonzalez <agonzalezf87@gmail.com>
2022-07-26 11:28:26 -0500

    Form sending adapting netlify forms.

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,23 +1,46 @@
+import { useRef } from 'react'
 import "../../styles/ContactForm.component.css"
 import { CtaButton } from "../CtaButton"
 import { FormInput } from "./FormInput"
 import "../../styles/ContactForm.component.css"
 
 function ContactForm({language}) {
-  /* const handleSubmit = (evt) => {
+  const form = useRef(null)
+  const handleSubmit = (evt) => {
+    const formData = new FormData(form.current)
+    const conctactData = {
+      'name': formData.get('name'),
+      'telephone': formData.get('telephone'),
+      'email': formData.get('email'),
+      'subject': formData.get('subject'),
+      'message': formData.get('message'),
+    }
+    
+    fetch('/#Contact', {
+      method: 'POST',
+      headers: {
+        'Content_Type': 'application/x-www-form-urlencoded'
+      },
+      body: conctactData
+    })
+    .then(() => {
+      form.current.reset()
+      alert(language === 'en' ? "Thanks for writing, I'll be in touch soon." : "Gracias por escribir, pronto me pondré en contacto.")
+    })
+    .catch(error => alert(error))
     evt.preventDefault()
-  } */
+  }
   return (
-    <form className="ContactForm" name="ContactForm" method="POST" data-netlify="true" onSubmit="submit" data-netlify-honeypot="bot-field" >
+    <form ref={form} className="ContactForm" name="ContactForm" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={handleSubmit}>
       <div className="ContactForm__wrapper">
         <input type='hidden' name='form-name' value='ContactForm' />
-        <div hidden><input name="bot-field" /></div>
         <FormInput fiID='name' fiType='text' plcHolder={language === 'en' ? 'Full Name' : 'Nombre Completo'} isRequired={true} />
         <FormInput fiID='telephone' fiType='tel' plcHolder={language === 'en' ? 'Phone Number' : 'Número Telefónico'} isRequired={false} />
         <FormInput fiID='email' fiType='email' plcHolder={language === 'en' ? 'E-Mail' : 'Correo Elecrónico'} isRequired={true} />
         <FormInput fiID='subject' fiType='text' plcHolder={language === 'en' ? 'Subject' : 'Asunto'} isRequired={true} />
         <FormInput fiID='message' fiType='textarea' plcHolder={language === 'en' ? 'Your Message' : 'Su Mensaje'} isRequired={true} />
       </div>
+      <div hidden><input name="bot-field" /></div>
       <CtaButton ctaType={'send'} title={language === 'en' ? 'Send Message' : 'Enviar Mensaje'} />
     </form>
   )
