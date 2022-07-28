a3b9530cc1849f3324556bc21ebb2a48b657ed8f
Antonio Gonzalez <agonzalezf87@gmail.com>
2022-07-27 23:00:38 -0500

    Flags colors are now the slider's background. Explicit lang text showing.

--- src/components/Header/LangButton.jsx
+++ src/components/Header/LangButton.jsx
@@ -6,9 +6,9 @@ const LangButton = ({language, changeLang}) => {
   }
 
   return (
-    <div className="LangButton" title={language === 'en' ? 'Cambiar a español' : 'Change to english'} onClick={handleClick}>
+    <div className={language === 'en' ? "LangButton eng" : "LangButton spa"} title={language === 'en' ? 'Cambiar a español' : 'Change to english'} onClick={handleClick}>
       <div className={language === 'en' ? 'LangButton__wrapper' : 'LangButton__wrapper right'}>
-        <div className={language === 'en' ? "LangButton__slider en" : "LangButton__slider es"}></div>
+        <div className={language === 'en' ? "LangButton__slider en" : "LangButton__slider es"}>{language}</div>
       </div>
     </div>
   )
